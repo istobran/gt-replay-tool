84d431076f867de545688460b729620c129f8def
BangZ <i@bangz.me>
2016-02-01 19:23:31 +0800

    add tool function library

--- model/ReplayInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-interface ReplayInfo {
-  public var $mapName;
-  public var $players;
-  public var $version;
-  public function parseFile($filename);
-  public function parseData($data);
-}
- ?>
